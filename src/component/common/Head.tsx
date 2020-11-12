e7ae0feaefd6b845bcdd5d33747dd3c94c8f4f18
takurinton <takuirnton@gmail.com>
2020-11-12 19:30:52 +0900

    change head

--- src/component/common/Head.tsx
+++ src/component/common/Head.tsx
@@ -9,13 +9,13 @@ export const HtmlHead = ({ title, description, image, url }: HeadProps) => {
       <meta property="og:description" content={description} />
       <meta property="og:type" content="blog" />
       <meta property="og:url" content={url} />
-      <meta property="og:image" content={image} />
+      <meta property="og:image" content={'https://takurinton.vercel.app/me.jpeg'} />
       <meta property="og:site_name" content={title} />
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:url" content={image} />
       <meta name="twitter:title" content={title} />
       <meta name="twitter:description" content={description} />
-      <meta name="twitter:image" content={image} />
+      <meta name="twitter:image" content={'https://takurinton.vercel.app/me.jpeg'} />
       <link rel="canonical" href={url} />
       <link rel="shortcut icon" href={"me.jpeg"} />
       <link rel="apple-touch-icon" href={"me.jpeg"} />
