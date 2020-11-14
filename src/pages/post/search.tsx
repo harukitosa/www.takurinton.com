5caa8599995177cd7323bb3081f204559ea36553
takurinton <takuirnton@gmail.com>
2020-11-15 02:44:39 +0900

    add Morpheme dissected

--- src/pages/post/search.tsx
+++ src/pages/post/search.tsx
@@ -4,27 +4,34 @@ import React, {
     useState,
   } from 'react'
 
+const k = require('kuromoji')
+
 const Search = () => {
     const recognition = useRef<Recognition>();
-    const [input, setInput] = useState<string>('')
-    const [initial, setInitial] = useState<boolean>(true)
-    const [loading, setLoading] = useState<boolean>(false)
-    const [post, setPost] = useState([])
-    
+    // const [input, setInput] = useState<string>('')
+    // const [initial, setInitial] = useState<boolean>(true)
+    // const [loading, setLoading] = useState<boolean>(false)
+    // const [post, setPost] = useState([])
 
     const state = {
         content: ''
     }
 
     const onStart = () => {
-        setLoading(true)
-        setInitial(false)
         const r = (recognition.current = new Recognition());
         r.onFinal = (c) => {
         r.toggle()
         state.content = c
-        console.log(c)
-    } }
+
+        k.builder({ dicPath: "/dict" }).build((err, tokenizer) => {
+            if(err){
+              console.log(err)
+            } else {
+              const tokens = tokenizer.tokenize(state.content)
+              console.log(tokens)
+            }
+          })
+    }}
 
     const onStop = () => {
         const r = (recognition.current = new Recognition());
