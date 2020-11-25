da40c07000414daa6c7fdbc4b20c7f8f5ef35113
Takumi Katayama <50351271+takurinton@users.noreply.github.com>
2020-11-25 19:11:14 +0900

    change my name

--- src/component/profile/mine/MineContent.tsx
+++ src/component/profile/mine/MineContent.tsx
@@ -5,7 +5,7 @@ export const MineContent = (mine: MineProps) => {
     return (
         <div className={css.component}>
             <img src="https://www.takurinton.com/me.jpeg" alt="me"/>
-            <h3>たくりんとん</h3>
+            <h3>Takumi Katayama</h3>
             <p>{mine.content}</p>
             <p>Web制作のお仕事募集してます。<br />
                 takurinton[at]takurinton.com、<br />
