7a56f669abf5c8988310a0cb2513c23972fcdf00
Takumi Katayama <takurinton@takurinton.com>
2021-04-17 22:17:24 +0900

    update

--- src/pages/me/index.tsx
+++ src/pages/me/index.tsx
@@ -168,33 +168,36 @@ const wiki_ja = `
 
 # インターンシップ 
 - **株式会社シスネクト**
-  - 2020/2
+  - 2020/02
   - システム開発
   - PHP, MySQL, 自社サーバ
 - **株式会社D2C**
-  - 2020/2
+  - 2020/02
   - インターネット広告の開発
   - Flask/Python
 - **株式会社VOYAGE GROUP**
-  - 2020/8
+  - 2020/08
   - チームでの開発
   - インプット、アウトプットの両方を行う1ヶ月のインターンシップ
   - preact/JavaScript, Go, AWS
   - [blog](https://www.takurinton.com/post/17)
 - **株式会社サイバーエージェント**
-  - 2020/9
+  - 2020/09
   - 就業型インターン
   - Ameba広告のDSPのJSONの処理を高速化するタスク
   - Go
   - [blog](https://www.takurinton.com/post/18)
 - **株式会社楽天**
-  - 2020/9
+  - 2020/09
   - 2週間のチーム開発
   - React/JavaScript, Django/Python
   - [blog](https://www.takurinton.com/post/19)
 - **株式会社VOYAGE GROUP**
-  - 2021/2 ~ ???
+  - 2021/02 ~ 2021/06(予定)
   - 内定者インターン
+- **株式会社プレイド**
+  - 2021/07(予定) ~ ???
+  - フロントエンドエンジニア
 
 # サイト
 - [ブログ](/)
@@ -203,7 +206,7 @@ const wiki_ja = `
 - [wantedly](https://www.wantedly.com/id/takurinton)
 - [qiita](https://qiita.com/takurinton)
 - [facebook](https://www.facebook.com/takurinton)
-- [instagram](https://www.instagram.com/___katayama___/)
+- [instagram](https://www.instagram.com/takurinton/)
 `
 
   const wiki_en = `
@@ -322,32 +325,35 @@ We will conduct research to bring out the relevance there.
 
 # internship 
 - **Sysnect Inc.**
-  - 2020/2
+  - 2020/02
   - Web backend developmend
   - PHP, MySQL, On premises server
 - **D2C Inc.**
-  - 2020/2
+  - 2020/02
   - Ad technology
   - Flask/Python
 - **VOYAGE GROUP Inc.**
-  - 2020/8
+  - 2020/08
   - Team development
   - preact/JavaScript, Go, AWS
   - [blog](https://www.takurinton.com/post/17)
 - **CyberAgent Inc.**
-  - 2020/9
+  - 2020/09
   - Employment type
   - Ad technology of Ameba
   - Go
   - [blog](https://www.takurinton.com/post/18)
 - **Rakuten Inc.**
-  - 2020/9
+  - 2020/09
   - Team development
   - React/JavaScript, Django/Python
   - [blog](https://www.takurinton.com/post/19)
 - **VOYAGE GROUP Inc.**
-  - 2021/2 ~ ???
+  - 2021/02 ~ 2021/06(plans)
   - Intern as an informal candidate
+- **VOYAGE GROUP Inc.**
+  - 2021/07(plans) ~ ???
+  - Frontend developer
 
 
 # site
@@ -358,7 +364,7 @@ We will conduct research to bring out the relevance there.
 - [wantedly](https://www.wantedly.com/id/takurinton)
 - [qiita](https://qiita.com/takurinton)
 - [facebook](https://www.facebook.com/takurinton)
-- [instagram](https://www.instagram.com/___katayama___/)
+- [instagram](https://www.instagram.com/takurinton/)
 `
 
 export default Me
